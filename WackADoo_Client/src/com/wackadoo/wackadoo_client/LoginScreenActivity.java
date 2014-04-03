0a8f1762a7edca8751963ca42d04b8f6069d6558
Kevin Steinle <Kevin@5dlab.com>
2014-04-03 19:41:41 +0200

    Modified Login-Screen-> Added Graphics / Login Button / StoreButton

--- WackADoo_Client/src/com/wackadoo/wackadoo_client/LoginScreenActivity.java
+++ WackADoo_Client/src/com/wackadoo/wackadoo_client/LoginScreenActivity.java
@@ -2,35 +2,47 @@ package com.wackadoo.wackadoo_client;
 
 import com.example.wackadoo_webview.R;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
 public class LoginScreenActivity extends Activity{
 	
 		private Button loginButton;
+		private Button shopButton;
+		private AnimationDrawable loginButtonAnimation;
 	
 		@Override
 	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.activity_loginscreen);
 		    loginButton = (Button) findViewById(R.id.loginButton);
-		    loginButton.setOnClickListener(new View.OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					Intent intent = new Intent(LoginScreenActivity.this, WackadooWebviewActivity.class);
-			        startActivity(intent);
-				}
-			});
+		    shopButton = (Button) findViewById(R.id.shopButton);
+		    
+		    this.setUpButtonListeners();
+		    this.setUpLoginButtonAnimation();
+		    loginButtonAnimation.start();
 		}
 
+	   private void setUpLoginButtonAnimation() {
+		   loginButton.setBackgroundResource(R.drawable.animationlist_loginbutton);
+		   loginButtonAnimation = (AnimationDrawable) loginButton.getBackground();
+		}
 
-	   @Override
+	   private void setUpButtonListeners() {
+		   this.setUpLoginButton();
+		   this.setUpShopButton();
+	   }
+
+
+	@Override
 	   public boolean onCreateOptionsMenu(Menu menu) {
 	        getMenuInflater().inflate(R.menu.main, menu);
 	        return true;
@@ -44,4 +56,69 @@ public class LoginScreenActivity extends Activity{
 	        }
 		    return super.onOptionsItemSelected(item);
 	    }
+	   
+
+		private void setUpLoginButton() {
+			   loginButton.setOnTouchListener(new View.OnTouchListener() {
+					
+				@SuppressLint("NewApi")
+				@Override
+				   public boolean onTouch(View v, MotionEvent event) {
+					   switch ( event.getAction() ) {
+				    		case MotionEvent.ACTION_DOWN: 
+				    			{
+				    				loginButton.setBackground(getResources().getDrawable(R.drawable.title_play_button_active));
+				    				break;
+				    			}
+				    		case MotionEvent.ACTION_UP: 
+				    			{
+				    				setUpLoginButtonAnimation();
+				    				loginButtonAnimation.start();
+				    				break;
+				    			}
+					   }
+					   return false;
+					}
+			   });
+			   
+			   loginButton.setOnClickListener(new View.OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						Intent intent = new Intent(LoginScreenActivity.this, WackadooWebviewActivity.class);
+				        startActivity(intent);
+					}
+				});
+		}
+
+	   private void setUpShopButton() {
+		   shopButton.setOnTouchListener(new View.OnTouchListener() {
+				
+				@SuppressLint("NewApi")
+				@Override
+				   public boolean onTouch(View v, MotionEvent event) {
+					   switch ( event.getAction() ) {
+				    		case MotionEvent.ACTION_DOWN: 
+				    			{
+				    				shopButton.setBackground(getResources().getDrawable(R.drawable.title_shop_button_active));
+				    				break;
+				    			}
+				    		case MotionEvent.ACTION_UP: 
+				    			{
+				    				shopButton.setBackground(getResources().getDrawable(R.drawable.title_shop_button));
+				    				break;
+				    			}
+					   }
+					   return false;
+					}
+			   });
+			   
+		   shopButton.setOnClickListener(new View.OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						///// TODO: Insert Intent to Shop
+					}
+				});
+	}
 }
